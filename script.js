38a3b7324a163d6594032d2dd06b34a615867533
Pedro <jinnpheonix@protonmail.com>
2022-09-10 14:53:36 -0300

    slider / checkboxes / password gen

--- script.js
+++ script.js
@@ -1,10 +1,73 @@
+//Password display & Clipboard
 var clipboardButton = document.getElementById('clipboard-copy-button');
-var clipboardText = document.getElementById('generated-password');
-
-var length = document.getElementById('length-value');
+var generatedPwd = document.getElementById('generated-password');
 
 clipboardButton.onclick = function()
 {
-    console.log(clipboardText.innerHTML);
-    navigator.clipboard.writeText(clipboardText.innerHTML);
+    navigator.clipboard.writeText(generatedPwd.innerHTML);
+};
+
+//Range Slider
+var length = document.getElementById('length-value');
+
+var range = document.getElementById('range').oninput = function()
+{
+    var value = this.value-this.min;
+    length.innerHTML = value;
+};
+
+//Checkboxes
+const includeUppercase = document.querySelector('#include-uppercase');
+const includeLowercase = document.querySelector('#include-lowercase');
+const includeNumbers = document.querySelector('#include-numbers');
+const includeSymbols = document.querySelector('#include-symbols');
+
+
+//Submit & Password generation
+function defineString()
+{
+    var uppercaseIsChecked = includeUppercase.checked;
+    var lowercaseIsChecked = includeLowercase.checked;
+    var numbersIsChecked = includeNumbers.checked;
+    var symbolsIsChecked = includeSymbols.checked;
+
+    var uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    var numberChars = '0123456789';
+    var symbolChars = '!@#$%^&*()';
+
+    var finalChars = '';
+
+    if(uppercaseIsChecked === true)
+    {
+        finalChars = finalChars.concat(uppercaseChars);
+    }
+    if(lowercaseIsChecked === true)
+    {
+        finalChars = finalChars.concat(lowercaseChars);
+    }
+    if(numbersIsChecked === true)
+    {
+        finalChars = finalChars.concat(numberChars);
+    }
+    if(symbolsIsChecked === true)
+    {
+        finalChars = finalChars.concat(symbolChars);
+    }
+    return finalChars;
+};
+
+const generatePassword = document.getElementById('generate-password');
+
+generatePassword.onclick = function()
+{
+    chars = defineString();
+    var pwdLength = length.innerHTML;
+    var result = '';
+
+    for(var i = 0; i < pwdLength; i++)
+    {
+        result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    generatedPwd.innerHTML = result;
 };
